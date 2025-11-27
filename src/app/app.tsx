1c6ad7f4c5cabfea02ec9f500b7cdd6e75c6059c
joshuaedo <joshua.edo01@gmail.com>
2025-11-28 00:07:32 +0100

    fix: remove timer

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
-import useDate from '@/hooks/use-date';
-import { CountdownTimer } from '@/components/common/counter';
+// import useDate from '@/hooks/use-date';
+// import { CountdownTimer } from '@/components/common/counter';
 import Router from '@/components/layout/router';
 import Footer from '@/components/layout/footer';
 import Header from '@/components/layout/header';
@@ -9,21 +9,21 @@ import Providers from '@/providers';
 // import Cursor from '@/components/layout/cursor';
 
 export const App = () => {
-  const { isBirthday } = useDate();
+  // const { isBirthday } = useDate();
 
   return (
     <BrowserRouter>
       <div className='relative font-cormorant-garamond font-normal max-w-screen bg-primary text-tertiary'>
-        {isBirthday ? (
+        {/* {isBirthday ? (
           <CountdownTimer />
-        ) : (
+        ) : ( */}
           <Providers>
             <Header />
             <Router />
             {/* <Cursor /> */}
             <Footer />
           </Providers>
-        )}
+        {/* )} */}
       </div>
     </BrowserRouter>
   );
